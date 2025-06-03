0f2a0fc600aee522db70669c864c35c855ad8fbc
jordanharris0 <jordanharris1833@yahoo.com>
2025-06-03 15:01:23 -0500

    created POST and PATCH itme routes

--- server/routes/items.js
+++ server/routes/items.js
@@ -5,5 +5,45 @@ const router = express.Router();
 router.use(express.json());
 
 // Define your routes here
+router.post("/add", async (req, res) => {
+  try {
+    //check if item already exists
+    const item = await Item.findOne({ where: { name: req.body.name } });
+
+    //error handling
+    if (!item) {
+      //create item
+      const newItem = await Item.create(req.body);
+      //return
+      res.status(201).json({ message: "Item Created", newItem });
+    } else {
+      return res.status(400).json({ message: "Item already exists" });
+    }
+  } catch (error) {
+    console.error("Error creating item:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.patch("/:itemId", async (req, res) => {
+  try {
+    //find item
+    const item = await Item.findByPk(req.params.itemId);
+
+    //check if exists
+    if (!item) {
+      return res.status(404).json({ message: "Item not found" });
+    }
+
+    //update item
+    const updateItem = await item.update(req.body);
+
+    //return
+    res.status(200).json({ message: "Item Updated", updateItem });
+  } catch (error) {
+    console.error("Error updating item:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 module.exports = router;
