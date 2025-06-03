b310272a2727ec4519eeeaec14a7b5da64466213
madisondegrezia <madisondegrezia26@gmail.com>
2025-06-03 15:49:19 -0400

    Created routes to get all items and to delete an item by id

--- server/routes/items.js
+++ server/routes/items.js
@@ -5,5 +5,29 @@ const router = express.Router();
 router.use(express.json());
 
 // Define your routes here
+// GET /items
+router.get("/", async (req, res, next) => {
+  try {
+    const items = await Item.findAll();
+    res.send(items);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// DELETE /items/:id
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const item = await Item.findByPk(req.params.id);
+    if (item) {
+      const deletedItem = await item.destroy();
+      res.json(deletedItem);
+    } else {
+      res.status(404).json({ error: "Item not found" });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
