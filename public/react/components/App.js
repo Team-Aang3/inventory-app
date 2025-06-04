df9e9711ef75b5862d5c303a4887d94d0548f115
jordanharris0 <jordanharris1833@yahoo.com>
2025-06-04 14:37:59 -0500

    fixed extra component render in app.js

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -4,7 +4,7 @@ import { Item } from "./Item";
 import { ItemList } from "./ItemList";
 import { AddForm } from "./AddForm";
 import { UpdateForm } from "./UpdateForm";
-import './Item.css'
+import "./Item.css";
 
 // Prepend the API URL to any fetch calls.
 import apiURL from "../api";
@@ -30,7 +30,6 @@ function App() {
     <>
       <h1>Inventory App</h1>
       {/* Render the items */}
-      <Item/>
       <Routes>
         <Route path="/" element={<ItemList items={items} />} />
         <Route path="/:itemId" element={<Item />} />
