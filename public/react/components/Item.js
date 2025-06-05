8f55e9cb23005cce9f0b4be97a612b5dfa4e36f2
jordanharris0 <jordanharris1833@yahoo.com>
2025-06-05 14:54:58 -0500

    fixed type and navRoute in Items.js, fixed error handling in forms

--- public/react/components/Item.js
+++ public/react/components/Item.js
@@ -8,22 +8,22 @@ export function Item() {
   const { itemId } = useParams();
   const [item, setItem] = useState(null);
   const [error, setError] = useState(null);
-  const [isDeleting, setIsDeleting] = useState(false)
+  const [isDeleting, setIsDeleting] = useState(false);
   const apiURLWithId = `${apiURL}/items/${itemId}`;
 
   const handleDelete = async () => {
     setIsDeleting(true);
     setError(null);
-    try{
-    await fetch(apiURLWithId, {
-      method: "DELETE",
-    });
-    navigate("/items");
-  } catch (err){
-    setError('Failed to delete item. Please try again.')
-  } finally {
-    setIsDeleting(false);
-  }
+    try {
+      await fetch(apiURLWithId, {
+        method: "DELETE",
+      });
+      navigate("/");
+    } catch (err) {
+      setError("Failed to delete item. Please try again.");
+    } finally {
+      setIsDeleting(false);
+    }
   };
 
   useEffect(() => {
@@ -40,31 +40,30 @@ export function Item() {
   }, [itemId]);
 
   return item ? (
-    <div className="item-detail-card" style={{ display: 'flex', alignItems: 'flex-start'}}>
-      <img className="item-img" src={item.image} alt={item.name}/>
-       <div className='item-info'> <h2 className="item-name">{item.name}</h2>
-       <p className="item-category">Category: {item.category}</p>
-       <p className="item-price">${item.price}</p>
-      <p className="item-description">{item.description}</p> </div>
-     
-      <button onClick={() => navigate(-1)} className="btn">Back</button>
-      <button onClick={() => navigate(`/update/${itemId}`)} className="btn">Edit</button>
-      <button onClick={handleDelete} className="btn" diasbled={isDeleting}>{isDeleting ? 'Deleting' : 'Delete'}</button>
-      {error && <div className="error-msg">{error}</div>} </div>
+    <div
+      className="item-detail-card"
+      style={{ display: "flex", alignItems: "flex-start" }}
+    >
+      <img className="item-img" src={item.image} alt={item.name} />
+      <div className="item-info">
+        {" "}
+        <h2 className="item-name">{item.name}</h2>
+        <p className="item-category">Category: {item.category}</p>
+        <p className="item-price">${item.price}</p>
+        <p className="item-description">{item.description}</p>{" "}
+      </div>
+      <button onClick={() => navigate(-1)} className="btn">
+        Back
+      </button>
+      <button onClick={() => navigate(`/update/${itemId}`)} className="btn">
+        Edit
+      </button>
+      <button onClick={handleDelete} className="btn" disabled={isDeleting}>
+        {isDeleting ? "Deleting" : "Delete"}
+      </button>
+      {error && <div className="error-msg">{error}</div>}{" "}
+    </div>
   ) : (
     <div>Loading...</div>
   );
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
